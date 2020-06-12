b98675884e00f1bd6ea35e8fabc86fcb0151ec56
Chris Gibson <chrisg_aust@yahoo.com>
2020-06-12 12:57:43 +0100

    Additional step to use quicktype

--- PermutiveTools.rb
+++ PermutiveTools.rb
@@ -1,6 +1,15 @@
 require 'xcodeproj'
 
 module PermutiveTools
+    def PermutiveTools.generateSwift(jsonDirectory, swiftDirectory)
+        jsonFiles = Dir["#{jsonDirectory}/*.json"]
+        jsonFiles.each { |file|
+            swiftFilename = File.basename(file, ".json") + ".swift"
+            swiftFilename = swiftDirectory + "/" + swiftFilename
+            `quicktype --lang swift #{file} > #{swiftFilename}`
+        }
+    end
+    
     def PermutiveTools.sanitiseSwift(files)
         files.each { |file|
             codeText = File.read(file)
@@ -10,9 +19,6 @@ module PermutiveTools
             result = ""
             codeText.each_line do |line|
                 if line.start_with? "struct "
-                    if line.end_with? ": Codable {\n"
-                        line = "\n" + line.chomp(": Codable {\n") + " {\n"
-                    end
                     in_struct = true
                 elsif line.lstrip.start_with? "enum "
                     in_enum = true
@@ -28,10 +34,16 @@ module PermutiveTools
                     in_enum = false
                 end
             end
+            
             # Write sanitised content back
-            File.open(filename, 'w') { |file|
-                file.write(codeText)
-            }
+            begin
+              orginalFile = File.open(file, "w")
+              orginalFile.write(result)
+            rescue IOError => e
+              #some error occur, dir not writable etc.
+            ensure
+              orginalFile.close unless orginalFile.nil?
+            end
         }
     end
     
@@ -43,11 +55,9 @@ module PermutiveTools
         end
     end
     
-    def PermutiveTools.buildStructureList(directory)
-        fileList = Dir["#{directory}/*.swift"]
-        
+    def PermutiveTools.buildStructureList(swiftFiles)
         names = []
-        fileList.each { |file|
+        swiftFiles.each { |file|
             structDeclaration = `swiftc -dump-parse #{file} | grep "struct_decl"`.split("\n")
             declNames = []
             structDeclaration.each { |decl|
@@ -73,14 +83,12 @@ module PermutiveTools
           "",
           "import Permutive_iOS",
           "",
-          ""
         ].join("\n") + "\n"
         
         eventNames = []
         # Generate required extension for each defined structure
         structureList.each { |structure|
-            glueFileContent.concat("extension #{structure}: Codable {}\n")
-            if !structure.include? "."
+            if !structure.include?(".") && structure.end_with?("Event")
                 # If the structure name is top-level, keep as event name
                 eventNames << structure
             end
@@ -89,14 +97,13 @@ module PermutiveTools
         # Generate track function for each defined event name
         eventNames.each { |eventName|
             eventFunction = [
-            "\n",
-            "func track(event: #{eventName}) {",
+            "func track(event: #{eventName}) throws {",
             "    guard let encoded = try? JSONEncoder().encode(event),",
-            "        let dictionary = try? JSONSerialization.jsonObject(with: encoded) as? [String: Any]",
+            "        let dictionary = try? JSONSerialization.jsonObject(with: encoded) as? [String: AnyHashable]",
             "        else { return }",
             "",
-            "    let properties = EventProperties(dictionary)",
-            "    Permutive.track(event: \"#{eventName}\", properties: properties)",
+            "    let properties = try EventProperties(dictionary)",
+            "    try Permutive.track(event: \"#{eventName}\", properties: properties)",
             "}"
             ].join("\n") + "\n"
             glueFileContent.concat(eventFunction)
--- permutiveSDKHelper.rb
+++ permutiveSDKHelper.rb
@@ -2,16 +2,16 @@ require 'xcodeproj'
 require "./PermutiveTools"
 
 # Find the target directory for the Swift code.
-source = "./"
+jsonSource = "./"
 xcodeProject = ""
 xcodeTarget = ""
 
 def usage
-  puts("Usage: permutive-sdk-helper [src directory] <your.xcodeproj> <xcode target>")
+  puts("Usage: permutive-sdk-helper [json directory] <your.xcodeproj> <xcode target>")
 end
 
 if ARGV.count == 3
-  source = ARGV[0]
+  jsonSource = ARGV[0]
   xcodeProject = ARGV[1].chomp("/")
   xcodeTarget = ARGV[2]
 elsif ARGV.count == 2
@@ -22,28 +22,34 @@ else
   return
 end
 
-if !File.directory?(source) || !File.exist?("#{xcodeProject}/project.pbxproj")
+if !File.directory?(jsonSource) || !File.exist?("#{xcodeProject}/project.pbxproj")
     usage()
     return
 end
 
-puts("permutive-sdk-helper. Searching '#{source}'")
-source = source.chomp("/")
-swiftFiles = Dir["#{source}/*.swift"]
+puts("permutive-sdk-helper. Searching '#{jsonSource}'")
+jsonSource = jsonSource.chomp("/")
+swiftDirectory = File.dirname(xcodeProject).chomp("/") + "/Permutive"
+Dir.mkdir(swiftDirectory) unless File.exists?(swiftDirectory)
+
+# Generate swift code from JSON
+PermutiveTools.generateSwift(jsonSource, swiftDirectory)
 
 # Delete any existing generated file
-PermutiveTools.deleteExistingGlue("#{source}/PermutiveHelper.swift")
+PermutiveTools.deleteExistingGlue("#{swiftDirectory}/PermutiveHelper.swift")
 
+swiftFiles = Dir["#{swiftDirectory}/*.swift"]
 # Remove unwanted code from Swift files
 PermutiveTools.sanitiseSwift(swiftFiles)
 
 # Find the structures which need extensions
-structures = PermutiveTools.buildStructureList(source)
+structures = PermutiveTools.buildStructureList(swiftFiles)
 puts("Found #{structures.count} structs to extend")
 
 # Generate the PermutiveHelper.swift glue file
-PermutiveTools.generateGlueFile("#{source}/PermutiveHelper.swift", structures)
-puts("Generated #{source}/PermutiveHelper.swift")
+PermutiveTools.generateGlueFile("#{swiftDirectory}/PermutiveHelper.swift", structures)
+puts("Generated #{swiftDirectory}/PermutiveHelper.swift")
+swiftFiles = Dir["#{swiftDirectory}/*.swift"]
 
 # Add all swift files to project
 project = Xcodeproj::Project.open(xcodeProject)
